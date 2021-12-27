daf15eec3386ae7e587a0b247f6ed9fa27b32431
vikramvimala <vikram.s@siemens.com>
2021-12-27 11:12:43 +0530

    Updated with webserver

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+    required_version = ">= 0.12"
+    backend "s3" {
+        bucket = "myapp-bucket"
+        key = "myapp/state.tfstate"
+        region = "eu-west-3"
+    }
+}
+
+provider "aws" {
+    region = "eu-west-3"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = var.vpc_cidr_block
+
+  azs             = [var.avail_zone]
+  public_subnets  = [var.subnet_cidr_block]
+  public_subnet_tags = { Name = "${var.env_prefix}-subnet-1" }
+
+  tags = {
+    Name = "${var.env_prefix}-vpc"
+  }
+}
+
+
+module "myapp-server" {
+    source = "./modules/webserver"
+    vpc_id = module.vpc.vpc_id
+    my_ip = var.my_ip
+    env_prefix = var.env_prefix
+    image_name = var.image_name
+    public_key_location = var.public_key_location
+    instance_type = var.instance_type
+    subnet_id = module.vpc.public_subnets[0]
+    avail_zone = var.avail_zone
+}
